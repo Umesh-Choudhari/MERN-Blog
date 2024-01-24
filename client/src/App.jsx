05469ffb99c90df06d2692f8c98b4981bcc9476c
Ramesh <ramesh.choudhari@cctech.co.in>
2024-01-24 15:26:21 +0530

    Created header section

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,17 +6,19 @@ import SignIn from './Pages/SignIn'
 import SignUp from './Pages/SignUp'
 import DashBoard from './Pages/DashBoard'
 import Projects from './Pages/Projects'
+import Header from './Componenets/Header'
 
 export default function App() {
   return (
     <BrowserRouter>
+    <Header/>
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/about" element={<About />}></Route>
         <Route path="/signIn" element={<SignIn />}></Route>
         <Route path="/signUp" element={<SignUp />}></Route>
         <Route path="/dashboard" element={<DashBoard />}></Route>
-        <Route path="/dashboard" element={<Projects />}></Route>
+        <Route path="/Projects" element={<Projects />}></Route>
       </Routes>
     </BrowserRouter>
   )
